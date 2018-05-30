4282b8aaec28af011b27a1989e245d006047ff87
Nicholas Day <nick@tutorialsbynick.com>
2018-05-29 22:04:53 -0400

    Add support for deleting members on the frontend

--- frontend/src/models/Member.js
+++ frontend/src/models/Member.js
@@ -1,117 +1,126 @@
-import r from '../utils/request'
-import m from 'mithril'
-import Signin from './Signin'
-import stream from 'mithril/stream'
+import r from "../utils/request";
+import m from "mithril";
+import Signin from "./Signin";
+import stream from "mithril/stream";
 
 const Member = {
-    list: new Map(),
-    search: '',
-    filtered_members: [],
-    setSearch(value) { 
-        Member.search = value
-        if (Member.search.trim().length == 0) {
-            Member.current.clear()
-        }
-        Member.filtered_members = Array.from(Member.list.values()).filter(member => member.name.toUpperCase().indexOf(Member.search.toUpperCase()) > -1)
-        if (Member.filtered_members.length == 1) {
-            console.log(Member.filtered_members[0].id)
-            Member.setCurrent(Member.filtered_members[0].id)
-        } else {
-            Member.current.clear()
-        }
-    },
-    current: {
-        id: stream(),
-        name: stream(),
-        team: stream(),
-        last_pizza: stream(),
-        signin: stream(),
-        pizza: stream(),
-        signedin: stream(),
-        clear() {
-            Member.current.id("")
-            Member.current.name("")
-            Member.current.team("")
-            Member.current.last_pizza("")
-            Member.current.pizza("")
-            Member.current.signin("")
-            Member.current.signedin("")
-        }
-    },
-    setCurrent(id) {
-        let i = Member.list.get(+id)
-        Member.current.id(i.id)
-        Member.current.name(i.name)
-        Member.current.team(i.team)
-        Member.current.pizza(i.last_pizza)
-        Member.current.last_pizza(i.last_pizza)
-        let signin = Signin.signin(i.id)
-        Member.current.signin(signin)
-        Member.current.signedin(signin !== undefined)
-    },
-    signining_in: false,
-    check_shown: false,
-    signin() {
-        Member.signining_in = true
-        return r({
-            url: `/members/${Member.current.id()}/signin`,
-            method: 'POST',
-            data: { pizza: Member.current.pizza() }
-        }).then(() => {
-            Member.signining_in = false
-            Member.check_shown = true
-            Member.setSearch('')
-            setTimeout(() => {
-                Member.check_shown = false
-                m.redraw()
-            }, 1000)
-        })
-    },
-    signout() {
-        Member.signining_in = true
-        return r({
-            url: `/members/${Member.current.id()}/signout`,
-            method: 'GET'
-        }).then(() => {
-            Member.signining_in = false
-            Member.check_shown = true
-            Member.setSearch('')
-            setTimeout(() => {
-                Member.check_shown = false
-                m.redraw()
-            }, 1000)
-            Member.setSearch('')
-        })
-    },
-    action() {
-        if (Member.current.signedin() !== undefined) {
-            return Member.signout()
-        } else {
-            return Member.signin()
-        }
-    },
-    add() {
-        let data = {
-            name: Member.current.name(),
-            team: +Member.current.team(),
-            last_pizza: "None"
-        }
-
-        return r({
-            url: "/members",
-            method: 'POST',
-            data: data
-        })
-    },
-    all() {
-        return r({
-            url: '/members',
-            method: 'GET'
-        }).then(data => {
-            Member.list = new Map()
-            data.members.forEach(member => Member.list.set(member.id, member))
-        })
+  list: new Map(),
+  search: "",
+  filtered_members: [],
+  setSearch(value) {
+    Member.search = value;
+    if (Member.search.trim().length == 0) {
+      Member.current.clear();
     }
-}
+    Member.filtered_members = Array.from(Member.list.values()).filter(
+      member =>
+        member.name.toUpperCase().indexOf(Member.search.toUpperCase()) > -1
+    );
+    if (Member.filtered_members.length == 1) {
+      console.log(Member.filtered_members[0].id);
+      Member.setCurrent(Member.filtered_members[0].id);
+    } else {
+      Member.current.clear();
+    }
+  },
+  current: {
+    id: stream(),
+    name: stream(),
+    team: stream(),
+    last_pizza: stream(),
+    signin: stream(),
+    pizza: stream(),
+    signedin: stream(),
+    clear() {
+      Member.current.id("");
+      Member.current.name("");
+      Member.current.team("");
+      Member.current.last_pizza("");
+      Member.current.pizza("");
+      Member.current.signin("");
+      Member.current.signedin("");
+    }
+  },
+  setCurrent(id) {
+    let i = Member.list.get(+id);
+    Member.current.id(i.id);
+    Member.current.name(i.name);
+    Member.current.team(i.team);
+    Member.current.pizza(i.last_pizza);
+    Member.current.last_pizza(i.last_pizza);
+    let signin = Signin.signin(i.id);
+    Member.current.signin(signin);
+    Member.current.signedin(signin !== undefined);
+  },
+  signining_in: false,
+  check_shown: false,
+  signin() {
+    Member.signining_in = true;
+    return r({
+      url: `/members/${Member.current.id()}/signin`,
+      method: "POST",
+      data: { pizza: Member.current.pizza() }
+    }).then(() => {
+      Member.signining_in = false;
+      Member.check_shown = true;
+      Member.setSearch("");
+      setTimeout(() => {
+        Member.check_shown = false;
+        m.redraw();
+      }, 1000);
+    });
+  },
+  signout() {
+    Member.signining_in = true;
+    return r({
+      url: `/members/${Member.current.id()}/signout`,
+      method: "GET"
+    }).then(() => {
+      Member.signining_in = false;
+      Member.check_shown = true;
+      Member.setSearch("");
+      setTimeout(() => {
+        Member.check_shown = false;
+        m.redraw();
+      }, 1000);
+      Member.setSearch("");
+    });
+  },
+  action() {
+    if (Member.current.signedin() !== undefined) {
+      return Member.signout();
+    } else {
+      return Member.signin();
+    }
+  },
+  add() {
+    let data = {
+      name: Member.current.name(),
+      team: +Member.current.team(),
+      last_pizza: "None"
+    };
 
-export default Member
+    return r({
+      url: "/members",
+      method: "POST",
+      data: data
+    });
+  },
+  delete(id) {
+    return r({
+      url: "/member/" + id,
+      method: "DELETE"
+    });
+  },
+  all() {
+    return r({
+      url: "/members",
+      method: "GET"
+    }).then(data => {
+      Member.list = new Map();
+      data.members.forEach(member => Member.list.set(member.id, member));
+    });
+  }
+};
+
+export default Member;
--- frontend/src/views/Admin.js
+++ frontend/src/views/Admin.js
@@ -1,103 +1,122 @@
-import m from 'mithril'
-import Member from '../models/Member'
-import Signin from '../models/Signin'
+import m from "mithril";
+import Member from "../models/Member";
+import Signin from "../models/Signin";
 
 function addMember(e) {
-    e.preventDefault()
-    Member.add().then(Member.current.clear).then(Member.all)
+  e.preventDefault();
+  Member.add()
+    .then(Member.current.clear)
+    .then(Member.all);
+}
+
+function deleteMember(id) {
+  return function(event) {
+    event.preventDefault();
+    Member.delete(id)
+      .then(Member.current.clear)
+      .then(Member.all);
+  };
 }
 
 function radio(name) {
-    return [
-        m(`input[type='radio'][name='data'][value='${name}']`,
-            { 
-                checked: Member.current.team() == name,
-                onclick: m.withAttr('value', Member.current.team)
-            }
-        ),
-        m(`label[for='${name}']`, name)
-    ]
+  return [
+    m(`input[type='radio'][name='data'][value='${name}']`, {
+      checked: Member.current.team() == name,
+      onclick: m.withAttr("value", Member.current.team)
+    }),
+    m(`label[for='${name}']`, name)
+  ];
 }
 
 const Admin = {
-    oninit() {
-        Member.all().then(m.redraw)
-        Signin.all().then(m.redraw)
-    },
-    view() {
-        return m("div.pa4", [
-            m('div', [
-                m('p', "Cheese: " + Signin.current.cheese),
-                m('p', "Peppers: " + Signin.current.peppers),
-                m('p', "Bacon: " + Signin.current.bacon),
-                m('p', "None: " + Signin.current.none)
-            ]),
-            m('div',
-                { onsubmit: addMember },
-                m('form.center.measure.measure-narrow-ns.pa2', [
-                    m("input.input-reset.w-100.pa2.ba.b--black-20[name='name'][placeholder='Name']",
-                        {
-                            oninput: m.withAttr('value', Member.current.name),
-                            value: Member.current.name()
-                        }
-                    ),
-                    m('div.mv3.flex.justify-around', [
-                        radio("731"),
-                        radio("5795"),
-                        radio("10195"),
-                        radio("6183"),
-                        radio("1533")
-                    ]),
-                    m("input.input-reset.lh-copy.white.b.ba.br3.bw0.bg-orange.ph3.pv2.f6.pointer[type='submit'][value='Add']")
-                ])
-            ),
-            m('table.w-100.center.mb4', [
-                m('thead',
-                    m('tr.stripe-dark', [
-                        m('th', 'ID'),
-                        m('th', 'Name'),
-                        m('th', 'Team'),
-                        m('th', 'Last Pizza'),
-                    ])
-                ),
-                m('tbody',
-                    Array.from(Member.list.values()).map(member => {
-                        return m('tr.stripe-dark', [
-                            m('td.pa2', member.id),
-                            m('td.pa2', member.name),
-                            m('td.pa2', member.team),
-                            m('td.pa2', member.last_pizza)
-                        ])
-                    })
-                )
-            ]),
-            m('table.w-100.center', [
-                m('thead',
-                    m('tr.stripe-dark', [
-                        m('th', 'ID'),
-                        m('th', 'Name'),
-                        m('th', 'Team'),
-                        m('th', 'Pizza'),
-                        m('th', 'DateTime in'),
-                        m('th', 'DateTime out')
-                    ])
-                ),
-                m('tbody',
-                    Object.values(Signin.list).map(signin => {
-                        let member = Member.list.get(signin.member_id)
-                        return m('tr.stripe-dark', [
-                            m('td.pa2', signin.id),
-                            m('td.pa2', member.name),
-                            m('td.pa2', member.team),
-                            m('td.pa2', signin.pizza),
-                            m('td.pa2', signin.date_in),
-                            m('td.pa2', signin.date_out)
-                        ])
-                    })
-                )
-            ])
+  oninit() {
+    Member.all().then(m.redraw);
+    Signin.all().then(m.redraw);
+  },
+  view() {
+    return m("div.pa4", [
+      m("div", [
+        m("p", "Cheese: " + Signin.current.cheese),
+        m("p", "Peppers: " + Signin.current.peppers),
+        m("p", "Bacon: " + Signin.current.bacon),
+        m("p", "None: " + Signin.current.none)
+      ]),
+      m(
+        "div",
+        { onsubmit: addMember },
+        m("form.center.measure.measure-narrow-ns.pa2", [
+          m(
+            "input.input-reset.w-100.pa2.ba.b--black-20[name='name'][placeholder='Name']",
+            {
+              oninput: m.withAttr("value", Member.current.name),
+              value: Member.current.name()
+            }
+          ),
+          m("div.mv3.flex.justify-around", [
+            radio("731"),
+            radio("5795"),
+            radio("10195"),
+            radio("6183"),
+            radio("1533")
+          ]),
+          m(
+            "input.input-reset.lh-copy.white.b.ba.br3.bw0.bg-orange.ph3.pv2.f6.pointer[type='submit'][value='Add']"
+          )
         ])
-    }
-}
+      ),
+      m("table.w-100.center.mb4", [
+        m(
+          "thead",
+          m("tr.stripe-dark", [
+            m("th", "ID"),
+            m("th", "X"),
+            m("th", "Name"),
+            m("th", "Team"),
+            m("th", "Last Pizza")
+          ])
+        ),
+        m(
+          "tbody",
+          Array.from(Member.list.values()).map(member => {
+            return m("tr.stripe-dark", [
+              m("td.pa2", member.id),
+              m("td.pa2", m("a", { onclick: deleteMember(member.id) }, "X")),
+              m("td.pa2", member.name),
+              m("td.pa2", member.team),
+              m("td.pa2", member.last_pizza)
+            ]);
+          })
+        )
+      ]),
+      m("table.w-100.center", [
+        m(
+          "thead",
+          m("tr.stripe-dark", [
+            m("th", "ID"),
+            m("th", "Name"),
+            m("th", "Team"),
+            m("th", "Pizza"),
+            m("th", "DateTime in"),
+            m("th", "DateTime out")
+          ])
+        ),
+        m(
+          "tbody",
+          Object.values(Signin.list).map(signin => {
+            let member = Member.list.get(signin.member_id);
+            return m("tr.stripe-dark", [
+              m("td.pa2", signin.id),
+              m("td.pa2", member.name),
+              m("td.pa2", member.team),
+              m("td.pa2", signin.pizza),
+              m("td.pa2", signin.date_in),
+              m("td.pa2", signin.date_out)
+            ]);
+          })
+        )
+      ])
+    ]);
+  }
+};
 
-export default Admin
+export default Admin;
